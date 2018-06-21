b53f3d7eccb6d9abb6ef785362b4aebe432bcb24
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2018-06-21 18:57:50 -0400

    connected files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  
+};
--- server.js
+++ server.js
@@ -1,26 +1,56 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
 
-//doesn't listen to port when typing node server.js, I'm not sure why.
-var PORT = 3000;
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
 
-var friends = [];
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
 
 //Not sure how this works, got it from the restaurant app.
-app.get("/api/friends", function(req, res) {
-    return res.json(friends);
-  });
+// app.get("/api/friends", function(req, res) {
+//     return res.json(friends);
+//   });
 
-  app.post("/api/friends", function(req, res) {
+//   app.post("/api/friends", function(req, res) {
     
-    var newfriend = req.body;
+//     var newfriend = req.body;
 
-          friends.push(newfriend);
-          res.json(true);
+//           friends.push(newfriend);
+//           res.json(true);
       
-      console.log(newfriend);
+//       console.log(newfriend);
       
-  });
+//   });
+
+//   app.listen(3000, function(){
+//     console.log("app listening on 3000");
+//   })
